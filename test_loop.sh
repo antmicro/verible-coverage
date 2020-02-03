71b1ccc78fb34e00c5a9262cd4f19d8c546bdc6c
Rafal Kapuscik <rkapuscik@antmicro.com>
2020-02-03 12:41:23 +0100

    Add travis config
    Move some code to test_loop script to be shared across CIs.
    
    Signed-off-by: Rafal Kapuscik <rkapuscik@antmicro.com>

--- test_loop.sh
+++ test_loop.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+rm -f failed.log
+
 for file in ./tests/*.sv; do
-	echo "------------"
-	echo "Testing file "$file
-	verible/bazel-bin/verilog/tools/lint/verilog_lint -lint_fatal -ruleset all $file
-	if [ $? -eq 0 ]; then
-		echo "FAIL: expected a lint violation"
-		echo $file >> failed.log
-	else
-		echo "OK"
-	fi
+    echo "------------"
+    echo "Testing file "$file
+    verible/bazel-bin/verilog/tools/lint/verilog_lint -lint_fatal -ruleset all $file
+    if [ $? -eq 0 ]; then
+        echo "FAIL: expected a lint violation"
+        echo $file >> failed.log
+    else
+        echo "OK"
+    fi
 done
 
+printf "\n%s\n%s\n" "Undetected lint violations in:" "$(cat failed.log)"
